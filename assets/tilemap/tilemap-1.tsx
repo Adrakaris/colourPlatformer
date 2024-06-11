dc44853b18d6c04a3b3b79ad93d7421adb0ce95f
Yijun H <me@yijun.hu>
2024-06-11 14:48:06 +0100

    update level

--- assets/tilemap/tilemap-1.tsx
+++ assets/tilemap/tilemap-1.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="tilemap-1" tilewidth="8" tileheight="8" tilecount="64" columns="8">
- <image source="tilemap-1.png" width="64" height="64"/>
+<tileset version="1.10" tiledversion="1.10.2" name="tilemap-1" tilewidth="8" tileheight="8" tilecount="128" columns="16">
+ <image source="tilemap-1.png" width="128" height="64"/>
 </tileset>
